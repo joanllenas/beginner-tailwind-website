d164e2677c7f974d7ae465a24abc4c5c29f12790
Joan Llenas <joan.llenas.maso@gmail.com>
2021-02-17 23:56:11 +0100

    Trying React

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-6xl bg-teal-400 text-teal-100">
+      Hello I am a website!
+    </div>
+  );
+}
+
+export default App;
